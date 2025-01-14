776c5aaad1797bf456b3d9d32cf7f93787f75652
Carlos R <crodriguezq91@gmail.com>
2025-01-14 14:55:27 -0500

    Update test for new Match Control Panel

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Scoreboard from './components/Scoreboard/Scoreboard'
 import { MatchModel } from './models/scoreboard';
 import { initialMatches } from './utils/constants';
 import { MatchesContext } from './context/MatchesContext';
+import { MatchControlPanel } from './components/MatchControlPanel/MatchControlPanel';
 
 function App() {
   const [matches, setMatches] = useState<MatchModel[]>(initialMatches);
@@ -13,6 +14,7 @@ function App() {
       <h1>Football Live Scoreboard</h1>
       <AddMatch /> 
       <Scoreboard />
+      <MatchControlPanel />
     </MatchesContext.Provider>
   )
 }
--- src/components/MatchControlPanel/MatchControlPanel.test.tsx
+++ src/components/MatchControlPanel/MatchControlPanel.test.tsx
@@ -1,10 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MatchesContext } from "../../context/MatchesContext";
 import { expect, vi, describe, it } from "vitest";
 import { MatchModel } from "../../models/scoreboard";
 import { MatchControlPanel } from "./MatchControlPanel";
 
 describe('MatchControlPanel', () => {
+  const mockUpdateScore = vi.fn();
+  const mockFinishMatch = vi.fn();
+
   const mockMatch: MatchModel = {
     id: '1',
     homeTeam: 'Spain',
@@ -37,5 +40,25 @@ describe('MatchControlPanel', () => {
     it('does not render anything when no matches exist', () => {
     renderWithContext([]);
     expect(screen.queryByText('Match Summary Manager')).not.toBeInTheDocument();
+  });
+
+  it('updates score when input changes', () => {
+    renderWithContext([mockMatch]);
+    
+    const homeScoreInput = screen.getAllByRole('spinbutton')[0];
+    fireEvent.change(homeScoreInput, { target: { value: '2' } });
+    expect(mockUpdateScore).toHaveBeenCalledWith(mockMatch.id, 2, mockMatch.homeScore);
+    
+    const awayScoreInput = screen.getAllByRole('spinbutton')[1];
+    fireEvent.change(awayScoreInput, { target: { value: '2' } });
+    expect(mockUpdateScore).toHaveBeenCalledWith(mockMatch.id, 2, mockMatch.awayScore);
+  });
+
+  it('finishes match when finish button is clicked', () => {
+    renderWithContext([mockMatch]);
+    
+    const finishButton = screen.getByText('Finish Match');
+    fireEvent.click(finishButton);
+    expect(mockFinishMatch).toHaveBeenCalledWith(mockMatch.id);
   });
 });
