f7390b339b200a572423280957bebcfa70a03930
Carlos R <crodriguezq91@gmail.com>
2025-01-14 09:48:49 -0500

    Add failing test for showing matches

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { describe, expect, it } from "vitest";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from "./App";
 
 describe('App', () => {
@@ -10,5 +11,13 @@ describe('App', () => {
 
     expect(title).not.toBeNull();
     expect(title?.textContent).toBe('Football Live Scoreboard');
-  })
+  });
+
+  it('renders the app with matches',  () => {
+    render(<App />)
+    expect(screen.getByText(/Spain/)).toBeInTheDocument();
+    expect(screen.getByText(/Brazil/)).toBeInTheDocument();
+    expect(screen.getByText('2')).toBeInTheDocument();
+    expect(screen.getByText('1')).toBeInTheDocument();
+  });
 })
