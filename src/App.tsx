508a48ec746796b9794abc057b0364a784c11a56
Carlos R <crodriguezq91@gmail.com>
2025-01-14 10:04:18 -0500

    Add changes to pass render matches test

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
-import { useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const matches = [
+    {
+      team1: 'Mexico',
+      team2: 'Canada',
+      score1: 0,
+      score2: 5,
+    },
+    {
+      team1: 'Spain',
+      team2: 'Brazil',
+      score1: 10,
+      score2: 2,
+    },
+    {
+      team1: 'Germany',
+      team2: 'France',
+      score1: 2,
+      score2: 2,
+    },
+    {
+      team1: 'Uruguay',
+      team2: 'Italy',
+      score1: 6,
+      score2: 6,
+    },
+    {
+      team1: 'Argentina',
+      team2: 'Australia',
+      score1: 3,
+      score2: 1,
+    },
+  ]
 
   return (
     <>
       <h1>Football Live Scoreboard</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='match-container'>
+        <div className='match-row'>
+          <h2>Spain</h2>
+          <h2>2</h2>
+          <span>-</span>
+          <h2>Brazil</h2>
+          <h2>1</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
