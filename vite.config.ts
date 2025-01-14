b122e19ac0647e9bdbb10ba8bd4cf6a326525691
Carlos R <crodriguezq91@gmail.com>
2025-01-14 09:49:20 -0500

    Fix issues for running tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Enables Vitest globals like `expect`
+    environment: 'jsdom', // Provides a DOM-like environment
+    setupFiles: './setupTests.ts', // Load global configurations for tests
+  },
+});
